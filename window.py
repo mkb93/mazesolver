7415eeb45bcd7c495c8119ae3c0c34a31ec5a41a
Marco Karim belahouane <nfk18@hotmail.co.uk>
2025-01-21 17:06:38 +0000

    added class point, class line and made sure they both work with window

--- window.py
+++ window.py
@@ -1,5 +1,19 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+  def __init__(self,x,y):
+    self.x = x
+    self.y = y
+
+class Line:
+  def __init__(self,point1, point2):
+    self.point1 = point1
+    self.point2 = point2
+  def draw(self, canvas, fill_color):
+    canvas.create_line(
+      self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill= fill_color, width=2
+    )
+
 class Window:
   def __init__(self, width, height):
     #initialise the window with paramaters.
@@ -22,9 +36,22 @@ class Window:
       self.redraw()
   def close(self):
     self.__running = False
+  
+  def draw_line(self, line, fill_color):
+    line.draw(self.__canvas, fill_color)
+
 
 
 if __name__ == "__main__":
+    # create window
     win = Window(800, 600)
+    # Create points
+    point1 = Point(11, 22)  # Replace with actual coordinates
+    point2 = Point(33, 45)  # Replace with actual coordinates
+
+    # Create a line using those points
+    line = Line(point1, point2)
+    win.draw_line(line ,'red')
+    
     win.wait_for_close()
   
