55c5edd4c4717e3410f33c3c33b7554e515780f9
Marco Karim belahouane <nfk18@hotmail.co.uk>
2025-01-23 11:52:08 +0000

    first constructor method attempt on maze class

--- window.py
+++ window.py
@@ -92,6 +92,27 @@ class Window:
   def draw_line(self, line, fill_color):
     line.draw(self.__canvas, fill_color)
 
+class Maze:
+  def __init__(
+    self,
+    x1,
+    y1,
+    num_rows,
+    num_cols,
+    cell_size_x,
+    cell_size_y,
+    win
+  ):
+    self._x1 = x1
+    self._y1 = y1
+    self._num_rows = num_rows
+    self._num_cols = num_cols
+    self._cell_size_x = cell_size_x
+    self._cell_size_y = cell_size_y
+    self._win = win
+  
+
+
 
 
 if __name__ == "__main__":
