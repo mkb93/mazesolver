6879f7e54ab6bab736d10ac944e51ff9e70af7d7
Marco Karim belahouane <nfk18@hotmail.co.uk>
2025-01-25 17:11:44 +0000

    added a test

--- tests.py
+++ tests.py
@@ -16,26 +16,27 @@ class Tests(unittest.TestCase):
       len(m1._cells[0]),
       num_cols,
     )
-  # def test_maze_2(self):
-  #   num_cols = 24
-  #   num_rows = 20
-    
-  #   m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
-
-  #   m1._create_cells()
-  #   self.assertEqual(
-  #     len(m1._cells),
-  #     num_rows,
-  #   )
-    # self.assertEqual(
-    #   len(m1._cells[0]),
-    #   num_cols,
-    # )
+  
     for i in range(num_rows):
       for j in range(num_cols):
         self.assertEqual(
           m1._cells[i][j].visited, False
         )
+  def test_maze_2(self):
+    num_cols = 24
+    num_rows = 20
+    
+    m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
+
+    m1._create_cells()
+    self.assertEqual(
+      len(m1._cells),
+      num_rows,
+    )
+    self.assertEqual(
+      len(m1._cells[0]),
+      num_cols,
+    )
 
 if __name__ == "__main__":
   unittest.main()
